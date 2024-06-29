f6a28bd68c321b0093cd1e789631a5c0ef282a9b
Kamila Krasuska <kamilakrasuska102@gmail.com>
2024-06-29 15:24:26 +0200

    Sponge

--- Assets/Whiteboard/Scripts/Whiteboard.cs
+++ Assets/Whiteboard/Scripts/Whiteboard.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Whiteboard : MonoBehaviour
 {
-
     public Texture2D texture;
-    public Vector2 textureSize=new Vector2(2048, 2048);
+    public Vector2 textureSize = new Vector2(2048, 2048);
+
     void Start()
     {
         var r = GetComponent<Renderer>();
-        texture= new Texture2D((int)textureSize.x,(int)textureSize.y);
+        texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
+        Color[] pixels = new Color[(int)textureSize.x * (int)textureSize.y];
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            pixels[i] = Color.white;
+        }
+        texture.SetPixels(pixels);
+        texture.Apply();
+        
         r.material.mainTexture = texture;
     }
-
 }
+
